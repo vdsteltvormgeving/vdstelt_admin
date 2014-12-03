b7a75ce7c392a2d4b83493e243883f911b863c91
THUIS <THUIS@145.44.82.99>
2014-12-03 15:16:17 +0100

    Merge origin/master
    Conflicts:
    	SysteemKlant/klantlogin.php

--- SysteemKlant/klantlogin.php
+++ SysteemKlant/klantlogin.php
@@ -77,7 +77,6 @@
                     }                        
                 }
                 ?>
-            </div>
             <footer>
                 <p class="copyright">Copyright © 2014 <b>Bens Development</b>, All Right Reserved.</p>
             </footer>
--- SysteemKlant/klantoverzicht.php
+++ SysteemKlant/klantoverzicht.php
@@ -45,6 +45,9 @@
                     }
                     ?>
                 </div>                        
+            <footer>
+                <p class="copyright">Copyright © 2014 <b>Bens Development</b>, All Right Reserved.</p>
+            </footer>
         </div>
     </body>
 </html>
