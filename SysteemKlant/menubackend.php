44180f919b9318052cf2924e969465fbdef58859
THUIS <THUIS@145.44.82.99>
2014-12-03 15:13:15 +0100

    Merge origin/master
    Conflicts:
    	SysteemKlant/klantlogin.php

--- SysteemKlant/menubackend.php
+++ SysteemKlant/menubackend.php
@@ -9,6 +9,16 @@
                         </ul>
                         </li>
                         <li class="rij"><a href="#">Facturen</a>
-                        <li class="rij"><a href="#">Uitloggen</a>
+                        <li class="rij"><a href="index.php?link=loguit">Uitloggen</a>
                     </ul>';
+                    
+                    if(isset($_POST["loguit"]))
+                    {
+                        $username=$_SESSION['username'];
+                        $password=$_SESSION['password'];                        
+                        $loguit=mysqli_prepare($link, "UPDATE User SET status='Offline' WHERE mail='$username'");
+                        mysqli_stmt_execute($loguit);                        
+                        session_destroy();
+                        header("location: klantlogin.php");
+                    }
 ?>
