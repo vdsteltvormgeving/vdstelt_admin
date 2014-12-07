d50802dcf1a7b922dbb8e97b12651fc10ba82987
Jeffrey <Jeffrey@Lurtze1>
2014-12-07 16:39:11 +0100

    Merge origin/master
    Conflicts:
    	SysteemAdmin/AdminLogin.php

--- SysteemAdmin/AdminTicketWijzigen.php
+++ SysteemAdmin/AdminTicketWijzigen.php
@@ -7,6 +7,19 @@ if ($_SESSION["login"] != 1) {
     session_unset();
     session_destroy();
 } else {
+    include 'link.php';
+    if (isset($_POST["wijzigen"])) {
+        $query = mysqli_prepare($link, 'INSERT INTO ticket (customer_id, creation_date, last_time_date, send_date, user_id, description ) VALUES (?,?,?,?,?,?)');
+        mysqli_stmt_bind_param($query, 'isssis', $CID, $creation, $lastchange, $send, $user_id, $desc);
+        $CID=$_POST["Customer_ID"];
+        $creation=$_POST["Creation Date"];
+        $lastchange=$_POST["Last Changed Date"];
+        $send=$_POST["Send Date to Hosting"];
+        $user_id=$_POST["User ID"];
+        $desc=$_POST["Description"];
+    } else {
+        
+    }
     ?>
     <html>
         <head>
@@ -38,20 +51,25 @@ if ($_SESSION["login"] != 1) {
                 <form action='' method='POST'>
                     <input type='text' value='$CID' name='Customer_ID'>
                     Klant: $comp
-                    <input type='text' value='$creationon' name='Creation Date'> 
+                    <input type='text' value='$creation' name='Creation Date'> 
                     <input type='text' value='$lastchange' name='Last Changed Date'> 
                     <input type='text' value='$send' name='Send Date to Hosting'> 
                     <input type='text' value='$userid' name='User ID'> 
                     Mail: $mail
-                    <input type='text' value='$category' name='Category'>
+                    <select id='Categorie' name='categorie'>
+                            <option value='$category'><?php echo $category; ?></option>
+                            <option value='website'>Website</option>
+                            <option value='cms'>CMS</option>
+                            <option value='hosting'>Hosting</option>
+                        </select>
                     <textarea cols='4' name='Description'>$desc</textarea>";
-                } $stmt2 = mysqli_prepare($link, "SELECT TIME, TEXT, U.mail FROM reaction R JOIN User U ON U.user_id = R.user_ID JOIN Ticket T ON T.ticket_id = R.ticket_id WHERE R.ticket_id = $ticket_id");
-                mysqli_stmt_bind_result($stmt1, $time, $text, $mail);
+                } $stmt2 = mysqli_prepare($link, "SELECT TIME, TEXT, U.mail reaction_id FROM reaction R JOIN User U ON U.user_id = R.user_ID JOIN Ticket T ON T.ticket_id = R.ticket_id WHERE R.ticket_id = $ticket_id");
+                mysqli_stmt_bind_result($stmt1, $time, $text, $mail, $reaction_id);
                 mysqli_stmt_execute($stmt1);
                 while (mysqli_stmt_fetch($stmt1)) {
 
                     echo"
-                    <textarea cols='4' name='Reaction'>$mail: $text  -$time</textarea>
+                    <textarea cols='4' name='Reaction[$reaction_id]'>$mail: $text  -$time</textarea>
                     <input type='submit' name='wijzigen' value='wijzigen' formaction=''>";
                 }
                 ?>
