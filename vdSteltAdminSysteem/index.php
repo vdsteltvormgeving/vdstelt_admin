04b56d1e8006553ab3c001bc07f5db2af0e1b91c
THUIS <THUIS@THUIS-PC>
2014-12-12 21:37:25 +0100

    Merge origin/master
    Conflicts:
    	SysteemKlant/klantlogin.php

--- vdSteltAdminSysteem/index.php
+++ vdSteltAdminSysteem/index.php
@@ -7,11 +7,18 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Van der Stelt Vormgeving - Administratie systeem</title>
+        <title>Van der Stelt Vormgeving - Administratie systeem</title><link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic' rel='stylesheet' type='text/css'>
+        <link href="adminstijl.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <?php
         include 'connect.php';
         ?>
+
+        <header>
+            <h1>Administratie systeem - Van der Stelt vormgeving</h1>
+        </header>
+        <content></content>
+        <footer></footer>
     </body>
 </html>
