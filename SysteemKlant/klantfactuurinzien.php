9824b7f537d704450ec28e9fb991fe766f9172bd
THUIS <THUIS@145.44.80.225>
2014-12-15 15:07:55 +0100

    Merge origin/master
    Conflicts:
    	SysteemKlant/klantlogin.php

--- SysteemKlant/klantfactuurinzien.php
+++ SysteemKlant/klantfactuurinzien.php
@@ -38,28 +38,28 @@
                 
                 ?> <!-- Dit maakt connectie met de database en zorgt voor de start van de inlogsessie -->
                 <div id="factuur">
-                    <p>Naam: <?php
+                    <p><?php
                         include"link.php";
                         $stmt = mysqli_prepare($link, "SELECT first_name, last_name FROM User WHERE mail='$username'");
                         mysqli_stmt_execute($stmt);
                         mysqli_stmt_bind_result($stmt, $fname, $lname);
                         while (mysqli_stmt_fetch($stmt))
                         {
-                            echo "$fname $lname <br>";
+                            echo "<label>Naam:</label>$fname $lname <br>";
                         }
                         
                         mysqli_close($link);
                         ?>
                     </p>
                     
-                    <p>Adres: <?php
+                    <p><?php
                     include "link.php";
                     $stat1 = mysqli_prepare($link, "SELECT street, house_number, city, kvk_number, btw_number FROM Customer WHERE customer_id = $user");
                 mysqli_stmt_execute($stat1);
                 mysqli_stmt_bind_result($stat1, $street, $housen, $city, $kvk, $btw);
                 while (mysqli_stmt_fetch($stat1)){
-                    echo "$street $housen <br>";
-                    echo "Woonplaats: $city";
+                    echo "<label>Adres:</label>$street $housen <br>";
+                    echo "<label>Woonplaats:</label>$city";
                     
                 }
                         
@@ -67,9 +67,9 @@
                     ?>
                 <p><?php
                     include "link.php";
-                    $stat2 = mysqli_prepare($link, "SELECT date FROM invoice WHERE user_id = $user");
+                    $stat2 = mysqli_prepare($link, "SELECT date, payment_completed FROM invoice WHERE user_id = $user");
                 mysqli_stmt_execute($stat2);
-                mysqli_stmt_bind_result($stat2, $date);
+                mysqli_stmt_bind_result($stat2, $date, $payment_completed);
                 while (mysqli_stmt_fetch($stat2)){
                     
                 }
@@ -79,9 +79,9 @@
                     {
                         $invoiceID = $invoice;
                     }
-                    echo "<label>Factuur nummer:</label><label>$invoiceID</label>";
+                    echo "<label>Factuurnummer:</label>$invoiceID";
                     echo "<br>";
-                    echo "<label>Datum:</label><label> $date</label>";
+                    echo "<label>Datum:</label>$date";
                     ?>
                 </p>
                 
@@ -92,23 +92,30 @@
                 $stmt3 = mysqli_prepare($link, "SELECT line_id, invoice_number, description, description2, amount, price, btw FROM line WHERE invoice_number = $invoiceID");
                 mysqli_stmt_execute($stmt3);
                 mysqli_stmt_bind_result($stmt3, $lineID, $IN, $D1, $D2, $amount, $price, $BTW);
-                echo "<table><th>Beschrijving</th><th>Aantal</th><th>prijs</th>";
+                echo "<table><th>Beschrijving</th><th>Aantal</th><th>Prijs</th>";
                 while (mysqli_stmt_fetch($stmt3)){
                    $total = $total + ($amount * $price); 
-                   echo "<tr><td>$D1</td><td>$amount</td><td>$price</td></tr>" ;
+                   echo "<tr><td>$D1</td><td>$amount</td><td>€ $price</td></tr>" ;
                    
                 }
                 $BTWsub = ($BTW/100)+1;
                 $totalincbtw = $total * $BTWsub;
                 $BTWtotal = $totalincbtw - $total;
                 
-                echo "<tr><td>Totaal</td><td>€ $total</td></tr>";
-                echo "<tr><td>BTW $BTW %</td><td>€ $BTWtotal</td></tr>";
-                echo "<tr><td>Totaal inc. btw</td><td>$totalincbtw</td></tr>";
-                echo "</table>";
+                echo "</table><br>";
+                echo "<label class='factuur'>Subtotaal</label>€ $total<br>";
+                echo "<label class='factuur'>BTW 21 %</label>€ $BTWtotal<br>";
+                echo "<label class='factuur'><strong>Totaal</strong></label>€ $totalincbtw";
+                echo $payment_completed;
                 ?>
                 </p>
-                <p> IBAN: NL 83 RABO 0344 4625 36 <br>
+                <p>IBAN: NL 83 RABO 0344 4625 36</p>
+                <?php if($payment_completed == ''){
+                    echo '<p>Deze factuur dient binnen 14 dagen op bovenstaande rekeningnummer t.n.v. D. van Beek<br> 
+                    o.v.v. factuurnummer en datum overgemaakt te zijn.</p>';
+                } else {
+                    echo '<p>Deze factuur is al betaald.</p>';
+                }?>
                     
                 <form class="knop_link" method="post" action="Klantfactuurverzicht.php">
                 <input type="submit" name="back" value="Terug">
