e9984bb54eaaba74f87f4aeb0e4756710ba2881a
THUIS <THUIS@THUIS-PC>
2014-12-21 15:16:18 +0100

    Merge origin/master
    Conflicts:
    	SysteemAdmin/AdminFactuuroverzicht.php
    	SysteemAdmin/menu.php

--- vdSteltAdminSysteem/index.php
+++ vdSteltAdminSysteem/index.php
@@ -16,9 +16,39 @@ and open the template in the editor.
         ?>
 
         <header>
-            <h1>Administratie systeem - Van der Stelt vormgeving</h1>
+            <h1>Administratie Systeem</h1>
+            <p>versie 1.0</p>
         </header>
-        <content></content>
+        <div id="content">
+            <div class="column_small column_line side-shadow">
+                <h1>Welkom</h1>
+                <p style="color:lightgrey; font-size:10px;">Saturday, December 13 2014, 1:13</p>
+                <p>Goedendag <b>Sander</b>, Hoe gaat het met u?</p>
+                <p>Dit adminstratie systeem staat weer voor uw klaar.</p>
+                <?php
+                   /*$mydate=getdate(date("U"));
+                   for($i=0; $i<$mydate[hours];$i++){
+                       $uur = $mydate[hours] + 1;
+                   }
+                    echo "<p> $mydate[weekday], $mydate[month] $mydate[mday] $mydate[year], $mydate[hours]+1 :$mydate[minutes] </p>";
+                    */
+                    ?>
+                <p>Klik hier onder om uit te loggen:<br>
+                <a href="#">Uitloggen</a></p>
+            </div>
+            <div class="column_normal column_line side-shadow">
+                <h1>Home</h1>
+                <p>Hier kunt u kiezen, deze zin moet lang zijn voor een test kijken wat er precies gaat gebeuren!</p>
+                <table>
+                    <tr>
+                        <td><a href="#">Klanten overzicht</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">Facturen overzicht</a></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
         <footer></footer>
     </body>
 </html>
